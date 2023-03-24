e1fe863b61071468f33a552843f67fb95d56afcc
PromzzyKoncepts <okechukwupromise638@gmail.com>
2023-03-24 10:00:47 +0100

    Modify file to fetch from current user

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,7 +4,7 @@ class GroupsController < ApplicationController
 
   # GET /groups or /groups.json
   def index
-    @groups = Group.all
+    @groups = Group.where(user_id: current_user.id)
     end
 
   # GET /groups/1 or /groups/1.json
@@ -32,7 +32,7 @@ class GroupsController < ApplicationController
         'source'=> "/icons/kids.png"
       },
       { 'name' => "Transportation", 
-        'source'=> "/icons/travel.png"
+        'source'=> "/icons/travels.png"
       },
       { 'name' => "Others", 
         'source'=> "/icons/others.png"
