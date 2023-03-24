5ffc473a9b32d028c52ab57f10078c6ec684bd57
PromzzyKoncepts <okechukwupromise638@gmail.com>
2023-03-24 11:45:53 +0100

    Write tests for group models

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -3,18 +3,18 @@ require 'rails_helper'
 RSpec.describe Group, type: :model do
   before(:each) do
     @user = User.create(name: 'User', email: '123@gmail.com', password: '123456', password_confirmation: '123456')
-    @food = Food.create(name: 'Food', measurement_unit: 'kg', price: 1.0, user: @user)
+    @group = Group.create(name: 'Food', icon: 'https://www.sliderrevolution.com/wp-content/uploads/2020/02/srlogo.png', user: @user)
   end
 
   describe 'validations' do
     it 'should be valid with valid attributes' do
-      expect(@food).to be_a(Food)
-      expect(@food).to be_valid
+      expect(@group).to be_a(Group)
+      expect(@group).to be_valid
     end
 
     it 'is valid with a name' do
-      @food.name = 'Food'
-      expect(@food).to be_valid
+      @group.name = 'Food'
+      expect(@group).to be_valid
     end
   end
 
